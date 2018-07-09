a9a1039d8f53c44052bca36df94ba78120f0aa1a
최형석 <chsss@choehyeongseog-ui-MacBook-Pro.local>
2018-07-09 12:47:38 +0900

    server initiation work

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+var db_config = require('./config/db-config.json');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/'});
+var fs  = require('fs');
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: db_config.host,
+  port: db_config.port,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database
+});
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/signup', function(req, res) {
+  
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  console.log(err);
+  res.send(err);
+});
+
+module.exports = app;
